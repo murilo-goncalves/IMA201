d011e4042f7f3b0d0b9d8aa51ac7e59e669574b8
Marina Lima <marinaml219@gmail.com>
2021-10-27 18:38:22 +0200

    Fixed errors in the Weights Map

--- projet.py
+++ projet.py
@@ -6,28 +6,31 @@ import os
 
 from PIL import Image
 
-def show(im):
-    im = cv2.resize(im, (960, 540)) 
-    cv2.imshow('Image',im)
+def show(im,titre='none'):
+    if im.shape[0]==3:
+        imnew=im.transpose(1,2,0)
+    else:
+        imnew=im
+
+    cv2.imshow('image', imnew)
     cv2.waitKey(10000)
-    cv2.destroyAllWindows()
 
 def weights_map(images):
     (w_c, w_s, w_e) = (1, 1, 1)
 
     weights = []
-    wsum = np.zeros(images[0].shape[:2]) #size of the image
+    wsum = np.zeros(images[0].shape[:2], dtype=np.float32)
 
     i = 0
 
     for im in images:
-        image = np.float32(im)/255 #normalizes each image
+        image = np.float32(im)/255
 
         # contrast
         src_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         laplacian = cv2.Laplacian(src_gray, cv2.CV_32F)
         cont = np.absolute(laplacian)
-       
+        
         # saturation
         hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
         hue,sat,val = cv2.split(hsv)
@@ -45,26 +48,27 @@ def weights_map(images):
         W = np.ones(image.shape[:2])
 
         W_cont = cont ** w_c
-        W = np.multiply(W, W_cont)
+        W = np.multiply(W, W_cont) 
 
         W_sat = sat ** w_s
         W = np.multiply(W, W_sat)
 
         W_exp = exp ** w_e
-        W = np.multiply(W, W_exp)
-    
+        W = np.multiply(W, W_exp) + 1e-12
+
         wsum = wsum + W
-
+        
         weights.append(W)
+        show(W*255,titre='Poids')
 
+    nonzero = wsum > 0
     for i in range(len(weights)):
-        weights[i]= weights[i]/wsum
-
-    # weights = np.array(weights)
-    # pil_image = Image.fromarray(weights, 'RGB')
-    # open_cv_image = np.array(pil_image)
-    # show(open_cv_image)
-    # weights = weights.tolist()
+        weights[i][nonzero]= weights[i][nonzero]/wsum[nonzero]
+        
+    weights = np.array(weights)
+    show(weights,titre='Poids')
+    weights = weights.tolist()
+        
     return weights
        
 def naive_fusion(images, weights):
@@ -73,12 +77,13 @@ def naive_fusion(images, weights):
     open_cv_image = np.array(pil_image)
     naive_image = open_cv_image[:, :, ::-1].copy() 
     for channel in range(3):
-        for i in range(len(weights)):
+        for i in range(len(images)):
             naive_image[:, :, channel] = naive_image[:, :, channel] + (weights[i] * images[i][:, :, channel])
     return naive_image
 
 def main(argv):
-    path = os.path.dirname(os.path.realpath(__file__)) + "/Images"
+    path = os.path.dirname(os.path.realpath(__file__)) + "/Images/"
+    plt.figure()
     files = [f for f in os.listdir(path)]
     images = []
 
@@ -88,7 +93,7 @@ def main(argv):
 
     W = weights_map(images)
     res_naive = naive_fusion(images, W)
-    show(res_naive)
+    show(res_naive,titre='naive')
 
 if (__name__ == '__main__'):
     main(sys.argv)
